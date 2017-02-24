010632d328fbba8d541255842eb336d2233b1754
Juggernaut93 <julianbrennan93@gmail.com>
2017-02-24 15:52:42 +0100

    Added random starting point generation to reach convergence

--- matlab_code/testIntegrationCon.m
+++ matlab_code/testIntegrationCon.m
@@ -1,4 +1,4 @@
-function [res, err, flag, robDegrees, intDegrees, intErr] = testIntegrationCon(x, y, port)
+function [res, err, flag, robDegrees, intDegrees, intErr] = testIntegrationCon(x, y, port, max_it)
     L1 = Link([0,0,0,pi/2]);
     L2 = Link([0,0,104.5,0]);
     L3 = Link([0,0,97.5,0]);
@@ -7,18 +7,36 @@ function [res, err, flag, robDegrees, intDegrees, intErr] = testIntegrationCon(x
     rob = SerialLink([L1, L2, L3, L4, L5],'name','Robottino');
 
     lims = rob.qlim;
-    lims(2,1) = 0;
+    lims(2,1) = -30;
+    lims(3,1) = deg2rad(-90);
+    lims(4,1) = deg2rad(-160);
     rob.qlim = lims;
 
     dest = [x, y, -35];
     
-    [res, err, flag] = ikconnorot(rob, transl(dest), zeros(1,5));
+    it = 0;
+    err = Inf;
+    while it < max_it && err > 1
+        q0(1) = 0;
+        q0(2) = 45 + rand() * 40;
+        q0(3) = -(rand() * 70);
+        q0(4) = 0 -(rand() * 90);
+        q0(5) = 0;
+        [res, err, flag] = ikconnorot(rob, transl(dest), deg2rad(q0));
 
-    if flag < 0
-        error('È zumpat tutte cose');
+        if flag < 0
+            error('È zumpat tutte cose');
+        end
+        it = it + 1;
+        disp(it);
     end
-
+    
+    if err > 1
+        error('Convergence not reached');
+    end
+    disp(q0);
     res = mod(res + pi, 2*pi) - pi;
-
-    [robDegrees, intDegrees, intErr] = moveRobotDH(rad2deg(res),port);
+    topass = rad2deg(res);
+    topass(6) = 90;
+    %[robDegrees, intDegrees, intErr] = moveRobotDH(topass,port);
 end
